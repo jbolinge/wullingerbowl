d7729fc1cf2028c6e421a137ecf345c3f9d37330
jbolinge <jbolinge@hotmail.com>
2018-12-13 21:09:32 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -7,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={ logo } className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
